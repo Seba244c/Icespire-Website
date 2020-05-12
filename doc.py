5797a6eac1d3047e7c9ef62121d84900d62661af
Sebastian Snoer <23051740+Seba244c@users.noreply.github.com>
2020-05-12 14:56:44 +0200

    Delete old file

--- doc.py
+++
@@ -1,4 +0,0 @@
-import shutil
-import os
-os.rmdir('E:\\Code\\Project Icespire\\Website\\doc')
-shutil.copytree("E:\\Code\\Project Icespire\\Icespire\\doc","E:\\Code\\Project Icespire\\Website\\doc")
