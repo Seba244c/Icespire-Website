250ac8633f03900c42bb8dd4c8067edf9617acbe
Sebastian Snoer <seb.snoer@gmail.com>
2020-05-12 14:46:55 +0200

    Initial Commit

---
+++ doc.py
@@ -0,0 +1,4 @@
+import shutil
+import os
+os.rmdir('E:\\Code\\Project Icespire\\Website\\doc')
+shutil.copytree("E:\\Code\\Project Icespire\\Icespire\\doc","E:\\Code\\Project Icespire\\Website\\doc")
